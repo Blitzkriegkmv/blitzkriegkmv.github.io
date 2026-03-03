0af39ad4ca6f73a8d56c831703938cac80bbccb0
natyavidhan <natyavidhanbiswas10@gmail.com>
2026-03-03 17:25:29 +0530

    sponsors section

--- src/pages/Sponsors.jsx
+++ src/pages/Sponsors.jsx
@@ -4,48 +4,77 @@ import styles from './Sponsors.module.css'
 
 const sponsorsData = [
   {
-    name: 'Modulus',
-    college: 'Keshav Mahavidyalaya',
-    role: 'The Mathematicians',
-    image: '/media/sponsors_25/modulus.jpg',
-    link: 'https://www.instagram.com/mathsoc_kmv/'
-  },
-  {
     name: 'TechWhiz',
     college: 'PGDAV',
-    role: 'The Wizards',
     image: '/media/sponsors_25/techwhiz.jpeg',
-    link: 'https://www.instagram.com/techwhizpgdav/'
-  },
-  {
-    name: 'Turing',
-    college: 'ANDC',
-    role: 'The Pioneers',
-    image: '/media/sponsors_25/turing.jpg',
-    link: 'https://www.instagram.com/turing.andc/'
-  },
-  {
-    name: 'Quicksort',
-    college: 'Ramanujan College',
-    role: 'The Optimizers',
-    image: '/media/sponsors_25/quicksort.jpeg',
-    link: 'https://www.instagram.com/quiksort.rcdu/'
-  },
-  {
-    name: 'Sanganika',
-    college: 'DDUC',
-    role: 'The Innovators',
-    image: '/media/sponsors_25/sanganika.jpeg',
-    link: 'https://www.instagram.com/sanganika_dduc/'
+    link: 'https://www.instagram.com/techwhizpgdav'
   },
   {
     name: 'Sattva',
     college: 'Kalindi College',
-    role: 'The Essence',
-    image: '/media/sponsors_25/sattva.jpeg',
-    link: 'https://www.instagram.com/cs_sattva/'
-  }
+    image: '/media/sponsors_26/sattva.jpg',
+    link: 'https://www.instagram.com/cs_sattva'
+  },
+  {
+    name: 'Sanganika',
+    college: 'DDUC',
+    image: '/media/sponsors_26/sangnika.png',
+    link: 'https://www.instagram.com/sanganika_dduc'
+  },
+  {
+    name: 'Ramanugen',
+    college: '',
+    image: '/media/sponsors_26/ramanu.jpg',
+    link: 'https://www.instagram.com/ramanugen'
+  },
+  {
+    name: 'Modulus',
+    college: 'Keshav Mahavidyalaya',
+    image: '/media/sponsors_26/modulus.jpg',
+    link: 'https://www.instagram.com/mathsoc_kmv'
+  },
 ]
+
+const mediaPartners = [
+  {
+    name: 'Elixir Tech Community',
+    role: 'Media Partner',
+    image: '/media/sponsors_26/elixir.jpg',
+    link: 'https://www.instagram.com/elixirtechcommunity'
+  },
+  {
+    name: 'Final Revise',
+    role: 'Media Partner',
+    image: '/media/sponsors_26/finalrevise.png',
+    link: 'https://www.instagram.com/finalrevise/'
+  },
+]
+
+const SponsorCard = ({ sponsor, index }) => (
+  <a
+    href={sponsor.link}
+    target="_blank"
+    rel="noopener noreferrer"
+    className={styles.sponsorCard}
+    style={{ animationDelay: `${index * 0.1}s` }}
+  >
+    <div className={styles.cardGlow} />
+    <div className={styles.cardImageWrapper}>
+      <img src={sponsor.image} alt={sponsor.name} loading="lazy" />
+      <div className={styles.imageOverlay} />
+    </div>
+    <div className={styles.cardContent}>
+      {sponsor.role && <span className={styles.sponsorRole}>{sponsor.role}</span>}
+      <h3 className={styles.sponsorName}>{sponsor.name}</h3>
+      {sponsor.college && <p className={styles.sponsorCollege}>{sponsor.college}</p>}
+    </div>
+    <div className={styles.cardBorder} />
+    <div className={styles.visitLink}>
+      <span>Visit Portal</span>
+      <FontAwesomeIcon icon={faArrowRight} className={styles.linkArrow} />
+    </div>
+  </a>
+)
 
 function Sponsors() {
   return (
@@ -66,49 +95,25 @@ function Sponsors() {
 
       {/* Sponsors Grid */}
       <section className={styles.sponsorsSection}>
+        <div className={styles.sectionLabel}>
+          <span className={styles.sectionTag}>Sponsors</span>
+        </div>
         <div className={styles.sponsorsGrid}>
           {sponsorsData.map((sponsor, index) => (
-            <a
-              key={index}
-              href={sponsor.link}
-              target="_blank"
-              rel="noopener noreferrer"
-              className={styles.sponsorCard}
-              style={{ animationDelay: `${index * 0.1}s` }}
-            >
-              <div className={styles.cardGlow} />
-              <div className={styles.cardImageWrapper}>
-                <img src={sponsor.image} alt={sponsor.name} loading="lazy" />
-                <div className={styles.imageOverlay} />
-              </div>
-              <div className={styles.cardContent}>
-                <span className={styles.sponsorRole}>{sponsor.role}</span>
-                <h3 className={styles.sponsorName}>{sponsor.name}</h3>
-                <p className={styles.sponsorCollege}>{sponsor.college}</p>
-              </div>
-              <div className={styles.cardBorder} />
-              <div className={styles.visitLink}>
-                <span>Visit Portal</span>
-                <FontAwesomeIcon icon={faArrowRight} className={styles.linkArrow} />
-              </div>
-            </a>
+            <SponsorCard key={index} sponsor={sponsor} index={index} />
           ))}
         </div>
       </section>
 
-      {/* CTA Section */}
-      <section className={styles.ctaSection}>
-        <div className={styles.ctaContent}>
-          <h2 className={styles.ctaTitle}>Want to Join the Alliance?</h2>
-          <p className={styles.ctaText}>
-            The Upside Down grows stronger. We need more allies to fight the darkness.
-          </p>
-          <a 
-            href="mailto:blitz@keshav.du.ac.in" 
-            className="register-button"
-          >
-            Contact Us
-          </a>
+      {/* Media Partners */}
+      <section className={styles.sponsorsSection}>
+        <div className={styles.sectionLabel}>
+          <span className={styles.sectionTag}>Media Partners</span>
+        </div>
+        <div className={styles.mediaGrid}>
+          {mediaPartners.map((partner, index) => (
+            <SponsorCard key={index} sponsor={partner} index={index} />
+          ))}
         </div>
       </section>
     </main>
